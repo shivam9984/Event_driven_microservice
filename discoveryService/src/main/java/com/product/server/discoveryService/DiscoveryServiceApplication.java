2188f5c462dfe757982b4b3743632d1b52e6ea6b
-v <Shivam.5.Rai@niit.com>
2022-10-26 18:15:53 +0530

    upfdated

---
+++ discoveryService/src/main/java/com/product/server/discoveryService/DiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.product.server.discoveryService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServiceApplication.class, args);
+	}
+
+}
---
+++ discoveryService/src/test/java/com/product/server/discoveryService/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.product.server.discoveryService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
